8135b1295fbc329b102040ce7c713f8844d3929e
Frakelx <frakel.x@gmail.com>
2015-12-04 10:13:10 +0800

    figures， without Sep. 2015

--- EmpericalResearch/OrderBookConservativeParallel.m
+++ EmpericalResearch/OrderBookConservativeParallel.m
@@ -2,7 +2,7 @@ clc;clear;
 
 sourcePath = '.\Index Future Tick Data\TruncatedData';
 
-scale = [1];
+scale = [1, 5, 15, 30, 60, 300, 900, 1800];
 beta = zeros(length(scale),1);
 rsquare = zeros(length(scale),1);
 for iscale = 1:length(scale)
@@ -11,26 +11,35 @@ for iscale = 1:length(scale)
     
     timeSteps = seconds(timeScale);
     
-    files = dir([sourcePath,'\201501*.mat']);
+    files = dir([sourcePath,'\201508*.mat']);
+%     files2 = dir([sourcePath,'\201505*.mat']);
+%     files3 = dir([sourcePath,'\201506*.mat']);
+%     
+%     fields = fieldnames(files1);
+%     temp = [struct2cell(files1), struct2cell(files2), struct2cell(files3)];
+%     files = cell2struct(temp,fields);
+%     clear files1 files2 files3 temp
     
-    outflow = zeros(14400/timeScale,length(files));
+%     timeFlag = [datetime(0,0,0,9,30,0.0,'Format','HH:mm:ss:SSS'),datetime(0,0,0,11,30,0.0,'Format','HH:mm:ss:SSS'); ...
+%         datetime(0,0,0,13,0,0.0,'Format','HH:mm:ss:SSS'),datetime(0,0,0,15,0,0.0,'Format','HH:mm:ss:SSS')];
+%     timeGrid = [];
+%     for i = 1:2
+%         timeGrid = [timeGrid,timeFlag(i,1):timeSteps:timeFlag(i,2)];
+%     end
+    
+    timeGrid = datetime(0,0,0,11,0,0.0,'Format','HH:mm:ss:SSS'):timeSteps:datetime(0,0,0,11,30,0.0,'Format','HH:mm:ss:SSS');
+    N = length(files);
+    
+    outflow = zeros((length(timeGrid)-1),N);
     inflow = outflow;
     
-    timeFlag = [datetime(0,0,0,9,30,0.0,'Format','HH:mm:ss:SSS'),datetime(0,0,0,11,30,0.0,'Format','HH:mm:ss:SSS'); ...
-        datetime(0,0,0,13,0,0.0,'Format','HH:mm:ss:SSS'),datetime(0,0,0,15,0,0.0,'Format','HH:mm:ss:SSS')];
-    timeGrid = [];
-    for i = 1:2
-        timeGrid = [timeGrid,timeFlag(i,1):timeSteps:timeFlag(i,2)];
-    end
-    
-    N = length(files);
     display(sprintf('***Orderbook Conservation Analysis: time scale %d seconds***', timeScale))
     tic;
     parfor fIndex = 1:N
         x = load([sourcePath,'\',files(fIndex).name]);
         data = x.data;
         counter = 1;
-        tempIn = zeros(14400/timeScale,1);
+        tempIn = zeros((length(timeGrid)-1),1);
         tempOut = tempIn;
         for tIndex = 1:length(timeGrid)-1
             if(timeGrid(tIndex) == datetime(0,0,0,11,30,0.0,'Format','HH:mm:ss:SSS'))
@@ -63,7 +72,7 @@ for iscale = 1:length(scale)
     xlabel('Order inflow');
     ylabel('Order outflow');
     
-    if(timeScale<=60)
+    if(timeScale<60)
         title([num2str(timeScale),' seconds']);
     else
         title([num2str(timeScale/60),' minutes']);
@@ -72,3 +81,4 @@ for iscale = 1:length(scale)
     set(gca,'YLim',[0.8*min(outflow) 1.1*max(outflow)])
     toc
 end
+toc
